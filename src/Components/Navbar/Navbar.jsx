da417a6161d4bd6c8f1f04d849614841e7bef5f8
Mridul Hasan <125135774+mridul360@users.noreply.github.com>
2025-06-05 08:00:20 +0600

    Start

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import { FaUpwork } from "react-icons/fa6";
+import logo  from "../../assets/logo2.png";
+import Container from '../Layout/Container';
+
+const Navbar = () => {
+  return (
+    <div className=''>
+        <Container>
+            <div className='flex justify-between bg-black items-center px-[70px] rounded-b-xl'>
+      <div>
+        <img className='h-[80px]'  src={logo} alt="" />
+      </div>
+      <div>
+        <ul className='flex gap-[50px] text-white'>
+            <li><a href="">Home</a></li>
+            <li><a href="">Recent Work</a></li>
+            <li><a href="">About Me</a></li>
+            <li><a href="">Hire Me</a></li>
+            <li><a href="">Reviews</a></li>
+        </ul>
+      </div>
+      <div className='text-white flex gap-[20px]'>
+        <FaLinkedin className='text-2xl ' />
+        <FaUpwork  className='text-2xl ' />
+      </div>
+    </div>
+        </Container>
+    </div>
+  )
+}
+
+export default Navbar
