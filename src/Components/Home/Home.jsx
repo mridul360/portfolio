6594da10f0a14e947732311de6fa7980e54f534e
Mridul Hasan <125135774+mridul360@users.noreply.github.com>
2025-06-05 08:11:13 +0600

    solve

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Container from '../Layout/Container'
 
 const Home = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Container>
+        
+      </Container>
     </div>
   )
 }
