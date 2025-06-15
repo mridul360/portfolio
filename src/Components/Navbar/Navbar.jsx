724ba98766b5b2ba69e429133727ca0adb8be8fd
Mridul Hasan <125135774+mridul360@users.noreply.github.com>
2025-06-15 17:55:46 +0600

    work part

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-scroll';
 import { FaLinkedin } from "react-icons/fa";
 import { FaUpwork } from "react-icons/fa6";
 import logo from "../../assets/logo2.png";
@@ -6,38 +7,81 @@ import Container from '../Layout/Container';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full z-9999'
-    >
+    <div className='fixed w-full z-9999'>
       <Container>
-        <div className='flex justify-between  items-center px-[30px] rounded-b-xl'
+        <div 
+          className='flex justify-between items-center px-[30px] rounded-b-xl'
           style={{
             background: 'rgba(23, 23, 23, 0.4)',
             boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
             backdropFilter: 'blur(5.4px)',
             WebkitBackdropFilter: 'blur(5.4px)',
             border: '1px solid rgba(23, 23, 23, 0.3)',
-          }}>
+          }}
+        >
           <div>
-            <a href="#home"><img  className='h-[80px]' src={logo} alt="" /></a>
+            <a href="#home"><img className='h-[80px]' src={logo} alt="" /></a>
           </div>
           <div>
-
-            <ul className='flex gap-[50px]  font-bold items-center'>
-              <li> <a href="#about" className="relative group text-white hover:text-black">About Me
-      <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span></a></li>
-              <li> <a href="#works" className="relative group text-white hover:text-black">Recent Works
-      <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white  w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span></a></li>
-              <li> <a href="#reviews" className="relative group text-white hover:text-black">Reviews
-      <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white  w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span></a></li>
-      <li> <a href="#hire" className="relative group text-white hover:text-black">Hire Me
-      <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white  w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span></a></li>
-              
+            <ul className='flex gap-[50px] font-bold items-center'>
+              <li>
+                <Link
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="relative group text-white hover:text-black cursor-pointer"
+                  activeClass="active"
+                >
+                  About Me
+                  <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="works"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="relative group text-white hover:text-black cursor-pointer"
+                  activeClass="active"
+                >
+                  Recent Works
+                  <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="reviews"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="relative group text-white hover:text-black cursor-pointer"
+                  activeClass="active"
+                >
+                  Reviews
+                  <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="hire"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="relative group text-white hover:text-black cursor-pointer"
+                  activeClass="active"
+                >
+                  Hire Me
+                  <span className="absolute left-1/2 -bottom-1 h-0.5 bg-white w-0 group-hover:w-full group-hover:left-0 transition-all duration-400"></span>
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Container from '../Layout/Container'
+import agenc from '../../assets/agenc.jpg'
+
 
 const Works = () => {
   return (
     <div id='works' className='h-[100vh] bg-[#14213d]'>
       <Container>
-        <div >
+        <div className='flex justify-between items-center p-30'>
+            <div className='px-5 py-5 rounded-2xl 'style={{
+            background: 'rgba(23, 23, 23, 0.4)',
+            boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+            backdropFilter: 'blur(5.4px)',
+            WebkitBackdropFilter: 'blur(5.4px)',
+            border: '1px solid rgba(23, 23, 23, 0.3)',
+          }}>
+            <div><img className='h-[150px]' src={agenc} alt="" /></div>
+            <div className='text-center mt-3'>
+                <h2 className='text-[24px] leading-[24px] mb-6'>Agenc</h2>
+                <p>Made with Html,Css and Js</p>
+                <a href=""><button className='w-full bg-amber-100 rounded-3xl py-2 mt-3 cursor-pointer'>Code</button></a>
+            </div>
+          </div>
+          
         </div>
       </Container>
     </div>
